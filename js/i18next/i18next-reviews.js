7a852504ca0aea9a3f2da5e49fb671478f8b5e13
Andres Rios <riosandres91@gmail.com>
2024-11-26 15:21:54 -0400

    chore: Reviews Page added

---
+++ js/i18next/i18next-reviews.js
@@ -0,0 +1,183 @@
+// Load translations
+const resources = {
+    en: {
+        translation: {
+            "brand_title": "Riba de Rivers",
+            "brand_subtitle": "Apartments",
+            "book_now": "Book Now",
+            "nav": {
+                "home": "Home",
+                "overview": "Overview",
+                "map": "Map",
+                "gallery": "Gallery",
+                "availability": "Availability",
+                "reviews": "Reviews",
+                "contact": "Contact"
+            },
+            "reviews_title": "Reviews",
+            "reviews": {
+                "jacobo_info": "Has been on Airbnb for 5 years",
+                "jacobo_date": "November 2024",
+                "jacobo_review": "Everything was great, thank you very much Gabriel! Highly recommended!",
+                "roberto_info": "Has been on Airbnb for 4 months",
+                "roberto_date": "November 2024",
+                "roberto_review": "Highly recommended, communication with the host was great.",
+                "susana_info": "Has been on Airbnb for 10 years",
+                "susana_date": "October 2024",
+                "susana_review": "Gabriel's apartment exudes peace. It was very clean and well-kept, and no detail was missing. The location is perfect and all his recommendations were spot on. The instructions to access the apartment are clear and he has been a very attentive host. We will definitely be back!",
+                "lukas_info": "Has been on Airbnb for 3 years",
+                "lukas_date": "October 2024",
+                "lukas_review": "Nice and clean, could arrive a couple of hours early. Perfect stay for a couple of days.",
+                "maria_info": "Has been on Airbnb for 10 years",
+                "maria_date": "September 2024",
+                "maria_review": "It was an incredible stay, I felt at home, a super comfortable apartment, the sheets and towels were super new and of hotel quality!!! Gabriel responds super fast and is always attentive to make sure your experience is the best possible. Super well located! I would definitely come back.",
+                "lidia_info": "Has been on Airbnb for 7 years",
+                "lidia_date": "September 2024",
+                "lidia_review": "The apartment has been recently renovated, all the sheets and towels are in perfect condition, as are the kitchen utensils. The bed and sofa are very comfortable and there is space for teleworking. Gabriel has been very kind, thank you for the guide of recommendations, it has been very helpful.",
+                "alba_info": "Has been on Airbnb for 8 years",
+                "alba_date": "September 2024",
+                "alba_review": "The house is great, you can tell it has been recently renovated. The sofa bed is very comfortable and has all the necessary (new) utensils. Everything was spotless. The location is perfect, it takes 2 minutes by car to the center of Ribadesella and is perfect for exploring the area. In addition, Gabriel sent us several recommendations and it was super easy to communicate with him. We were 3 people and we were very comfortable. 100% recommended."
+            },
+            "footer": {
+                "copyright": "2024 Riba de Rivers Apartments. All rights reserved."
+            }
+        }
+    },
+    fr: {
+        translation: {
+            "brand_title": "Riba de Rivers",
+            "brand_subtitle": "Appartements",
+            "book_now": "Réserver",
+            "nav": {
+                "home": "Accueil",
+                "overview": "Aperçu",
+                "map": "Carte",
+                "gallery": "Galerie",
+                "availability": "Disponibilité",
+                "reviews": "Avis",
+                "contact": "Contact"
+            },
+            "reviews_title": "Avis",
+            "reviews": {
+                "jacobo_info": "Est sur Airbnb depuis 5 ans",
+                "jacobo_date": "Novembre 2024",
+                "jacobo_review": "Tout était parfait, merci beaucoup Gabriel ! Hautement recommandé !",
+                "roberto_info": "Est sur Airbnb depuis 4 mois",
+                "roberto_date": "Novembre 2024",
+                "roberto_review": "Hautement recommandé, la communication avec l'hôte était excellente.",
+                "susana_info": "Est sur Airbnb depuis 10 ans",
+                "susana_date": "Octobre 2024",
+                "susana_review": "L'appartement de Gabriel respire la paix. Il était très propre et bien entretenu, aucun détail ne manquait. L'emplacement est parfait et toutes ses recommandations étaient pertinentes. Les instructions pour accéder à l'appartement sont claires et il a été un hôte très attentif. Nous reviendrons certainement !",
+                "lukas_info": "Est sur Airbnb depuis 3 ans",
+                "lukas_date": "Octobre 2024",
+                "lukas_review": "Propre et agréable, nous avons pu arriver quelques heures plus tôt. Parfait pour un séjour de quelques jours.",
+                "maria_info": "Est sur Airbnb depuis 10 ans",
+                "maria_date": "Septembre 2024",
+                "maria_review": "Le séjour était incroyable, je me suis senti chez moi, un appartement super confortable, les draps et serviettes étaient comme neufs et de qualité hôtelière !!! Gabriel répond très rapidement et est toujours attentif pour que votre expérience soit la meilleure possible. Super bien situé ! Je reviendrai sans hésiter.",
+                "lidia_info": "Est sur Airbnb depuis 7 ans",
+                "lidia_date": "Septembre 2024",
+                "lidia_review": "L'appartement a été récemment rénové, tous les draps et serviettes sont en parfait état, tout comme les ustensiles de cuisine. Le lit et le canapé sont très confortables et il y a de l'espace pour le télétravail. Gabriel a été très gentil, merci pour le guide de recommandations, il a été très utile.",
+                "alba_info": "Est sur Airbnb depuis 8 ans",
+                "alba_date": "Septembre 2024",
+                "alba_review": "La maison est superbe, on voit qu'elle a été récemment rénovée. Le canapé-lit est très confortable et tous les ustensiles nécessaires (neufs) sont disponibles. Tout était impeccable. L'emplacement est parfait, à 2 minutes en voiture du centre de Ribadesella, idéal pour explorer la région. De plus, Gabriel nous a envoyé plusieurs recommandations et la communication avec lui était super facile. Nous étions 3 et très à l'aise. 100% recommandé."
+            },
+            "footer": {
+                "copyright": "2024 Riba de Rivers Appartements. Tous droits réservés."
+            }
+        }
+    },
+    es: {
+        translation: {
+            "brand_title": "Riba de Rivers",
+            "brand_subtitle": "Apartamentos",
+            "book_now": "Reservar",
+            "nav": {
+                "home": "Inicio",
+                "overview": "Visión general",
+                "map": "Mapa",
+                "gallery": "Galería",
+                "availability": "Disponibilidad",
+                "reviews": "Reseñas",
+                "contact": "Contacto"
+            },
+            "reviews_title": "Reseñas",
+            "reviews": {
+                "jacobo_info": "Está en Airbnb desde hace 5 años",
+                "jacobo_date": "Noviembre 2024",
+                "jacobo_review": "Todo estuvo excelente, ¡muchas gracias Gabriel! ¡Muy recomendado!",
+                "roberto_info": "Está en Airbnb desde hace 4 meses",
+                "roberto_date": "Noviembre 2024",
+                "roberto_review": "Muy recomendado, la comunicación con el anfitrión fue excelente.",
+                "susana_info": "Está en Airbnb desde hace 10 años",
+                "susana_date": "Octubre 2024",
+                "susana_review": "El apartamento de Gabriel transmite paz. Estaba muy limpio y bien cuidado, no faltaba ningún detalle. La ubicación es perfecta y todas sus recomendaciones fueron acertadas. Las instrucciones para acceder al apartamento son claras y ha sido un anfitrión muy atento. ¡Definitivamente volveremos!",
+                "lukas_info": "Está en Airbnb desde hace 3 años",
+                "lukas_date": "Octubre 2024",
+                "lukas_review": "Bonito y limpio, pudimos llegar unas horas antes. Perfecto para unos días.",
+                "maria_info": "Está en Airbnb desde hace 10 años",
+                "maria_date": "Septiembre 2024",
+                "maria_review": "Fue una estancia increíble, me sentí como en casa, un apartamento súper cómodo, ¡las sábanas y toallas eran nuevas y de calidad hotelera! Gabriel responde súper rápido y siempre está atento para que tu experiencia sea la mejor posible. ¡Muy bien ubicado! Definitivamente volvería.",
+                "lidia_info": "Está en Airbnb desde hace 7 años",
+                "lidia_date": "Septiembre 2024",
+                "lidia_review": "El apartamento ha sido recientemente renovado, todas las sábanas y toallas están en perfecto estado, así como los utensilios de cocina. La cama y el sofá son muy cómodos y hay espacio para teletrabajar. Gabriel ha sido muy amable, gracias por la guía de recomendaciones, ha sido de gran ayuda.",
+                "alba_info": "Está en Airbnb desde hace 8 años",
+                "alba_date": "Septiembre 2024",
+                "alba_review": "La casa es genial, se nota que ha sido recientemente renovada. El sofá cama es muy cómodo y cuenta con todos los utensilios necesarios (nuevos). Todo estaba impecable. La ubicación es perfecta, a 2 minutos en coche del centro de Ribadesella, ideal para explorar la zona. Además, Gabriel nos envió varias recomendaciones y fue muy fácil comunicarse con él. Éramos 3 personas y estuvimos muy cómodos. 100% recomendado."
+            },
+            "footer": {
+                "copyright": "2024 Riba de Rivers Apartamentos. Todos los derechos reservados."
+            }
+        }
+    }
+};
+
+// Initialize i18next
+i18next.use(i18nextBrowserLanguageDetector).init({
+    resources, // your translation resources
+    fallbackLng: 'en',
+    debug: true
+}, function (err, t) {
+    updateContent();
+});
+
+// Change language and update the button with the selected language and flag
+function changeLanguage(lang) {
+    i18next.changeLanguage(lang, function () {
+        updateContent(); // Update the content on the page
+
+        // Update the button with the correct flag and language
+        const selectedLangElement = document.getElementById('languageDropdownDesktop');
+
+        let flagClass;
+        let languageText;
+
+        switch (lang) {
+            case 'en':
+                flagClass = 'flag-icon-gb';
+                languageText = 'EN';
+                break;
+            case 'es':
+                flagClass = 'flag-icon-es';
+                languageText = 'ES';
+                break;
+            case 'fr':
+                flagClass = 'flag-icon-fr';
+                languageText = 'FR';
+                break;
+            default:
+                flagClass = 'flag-icon-gb';
+                languageText = 'EN';
+        }
+
+        // Update the button's inner HTML
+        selectedLangElement.innerHTML = `<span class="flag-icon ${flagClass}"></span> ${languageText}`;
+    });
+}
+
+// Update the content on the page
+function updateContent() {
+    document.querySelectorAll('[data-i18n]').forEach(function (element) {
+        const key = element.getAttribute('data-i18n');
+        element.textContent = i18next.t(key);
+    });
+}
