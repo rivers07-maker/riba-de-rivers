3e9c60dabd68e26c2be1c1737d666e9566c3a1f2
flzozaya <flzozaya@gmail.com>
2025-01-03 15:31:52 -0800

    refactor: Udate success path/endpoint

--- api/home.py
+++ api/home.py
@@ -43,7 +43,7 @@ def contact():
 
             supabase.table('submissions').insert(new_submission).execute()
 
-            return redirect(url_for("home.success"))
+            return redirect('/success')
         except Exception as e:
             logging.error(f"Error handling POST request: {e}")
             return "Internal Server Error", 500
@@ -58,6 +58,6 @@ def contact():
             logging.error(f"Error handling GET request: {e}")
             return "Internal Server Error", 500
 
-@app.route("/success")
-def success():
-    return render_template("success.html")
+# @app.route("/success")
+# def success():
+#     return render_template("success.html")
