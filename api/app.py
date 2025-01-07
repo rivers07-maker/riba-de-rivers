7e62dcd48d53c3e43f04fb30cb8b7345f6ee7f47
Andres Rios <riosandres91@gmail.com>
2025-01-06 22:05:26 -0400

    Fix app files & Vercel config for propper Deployment (#2)
    Co-authored-by: Lorenzo Zozaya <flzozaya@gmail.com>

--- api/app.py
+++ api/app.py
@@ -1,10 +1,7 @@
 from flask import Flask
-from home import app as home_blueprint
+from .contact import blueprint as contact_blueprint
 
 app = Flask(__name__)
 
 # Register the home blueprint
-app.register_blueprint(home_blueprint, url_prefix='/')
-
-if __name__ == "__main__":
-    app.run(debug=True)
+app.register_blueprint(contact_blueprint, url_prefix='/')
--- api/wsgi.py
+++ api/wsgi.py
@@ -1,11 +1,13 @@
 import sys
 import os
+from .app import app
 
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 # Import the Flask app instance from app.py
 try:
-    from .app import app
+    if __name__ == "__main__":
+        app.run(debug=True)
 except Exception as e:
     print(f"Error importing app: {e}")
     raise
