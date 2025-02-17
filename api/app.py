1410cb33c209f844cab4d8e97172e4e16e3b601c
Andres Rios <riosandres91@gmail.com>
2025-02-17 13:05:35 -0400

    Add sample booking.py file
    Still working on the booking feature

--- api/app.py
+++ api/app.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from .contact import blueprint as contact_blueprint
+# from .booking import blueprint as booking_blueprint
 
 app = Flask(__name__)
 
-# Register the home blueprint
+# Register the contact blueprint
 app.register_blueprint(contact_blueprint, url_prefix='/')
+
+# Register the booking blueprint
+# app.register_blueprint(booking_blueprint, url_prefix='/')
--- api/booking.py
+++ api/booking.py
@@ -1,37 +1,27 @@
 from flask import request, redirect, render_template, Blueprint
-from supabase import create_client, Client
 from dotenv import load_dotenv
 import os
 import logging
 
-blueprint = Blueprint("contact", __name__, template_folder='../public')
+blueprint = Blueprint("booking", __name__, template_folder='../public')
 
 load_dotenv()
 
 # Configure logging
 logging.basicConfig(level=logging.DEBUG)
 
-# Replace these placeholders with your actual Supabase credentials
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
-try:
-    supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
-except Exception as e:
-    logging.error(f"Error creating Supabase client: {e}")
-    raise
-
-@blueprint.route("/contact", methods=["GET", "POST"])
-def contact():
+@blueprint.route("/booking", methods=["GET", "POST"])
+def booking():
     if request.method == "POST":
         try:
-            new_submission = {
+            new_booking = {
                 'name': request.form.get('name'),
                 'email': request.form.get('email'),
                 'comment': request.form.get('comment')
             }
 
-            logging.debug(f"New submission: {new_submission}")
+            logging.debug(f"New booking: {new_booking}")
 
             supabase.table('submissions').insert(new_submission).execute()
 
