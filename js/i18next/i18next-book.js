e34edafb5cf767d02d192a2f1e0d3ec9387bea7c
Andres Rios <riosandres91@gmail.com>
2025-03-04 16:16:37 -0400

    feat: Add language translations

--- js/i18next/i18next-book.js
+++ js/i18next/i18next-book.js
@@ -14,31 +14,18 @@ const resources = {
                 "reviews": "Reviews",
                 "contact": "Contact"
             },
-            "book_title": "Book your stay",
-            "who_button": "Who's coming?",
-            "when_button": "When's your trip?",
-            "payment_button": "Payment method",
-            "who": {
+            "calendar": {
+                "where": "Where",
+                "arrival": "Arrival",
+                "departure": "Departure",
+                "who": "Who"
+            },
+            "info": {
                 "name": "Name",
                 "phone": "Phone",
-                "email": "Email",
-                "guests1": "1 Guest",
-                "guests2": "2 Guests",
-                "guests3": "3 Guests",
-                "guests4": "4 Guests",
-                "pets": "Pets"
+                "email": "Email"
             },
-            "when": {
-                "arrival": "Arrival",
-                "departure": "Departure"
-            },
-            "pay": {
-                "name": "Name",
-                "card": "Card Number",
-                "expiration": "Expiration (mm/yy)",
-                "security": "Security Code",
-            },
-            "book": "Book",
+            "pay": "Pay",
             "footer": {
                 "copyright": "2024 Riba de Rivers Apartments. All rights reserved."
             }
@@ -58,31 +45,18 @@ const resources = {
                 "reviews": "Avis",
                 "contact": "Contact"
             },
-            "book_title": "Réservez votre séjour",
-            "who_button": "Qui vient?",
-            "when_button": "Quand partez-vous?",
-            "payment_button": "Méthode de paiement",
-            "who": {
+            "calendar": {
+                "where": "Où",
+                "arrival": "Arrivée",
+                "departure": "Départ",
+                "who": "Qui"
+            },
+            "info": {
                 "name": "Nom",
                 "phone": "Téléphone",
-                "email": "Email",
-                "guests1": "1 Invité",
-                "guests2": "2 Invités",
-                "guests3": "3 Invités",
-                "guests4": "4 Invités",
-                "pets": "Animaux de compagnie"
+                "email": "Email"
             },
-            "when": {
-                "arrival": "Arrivée",
-                "departure": "Départ"
-            },
-            "pay": {
-                "name": "Nom",
-                "card": "Numéro de carte",
-                "expiration": "Expiration (mm/aa)",
-                "security": "Code de sécurité",
-            },
-            "book": "Réserver",
+            "pay": "Payer",
             "footer": {
                 "copyright": "2024 Riba de Rivers Appartements. Tous droits réservés."
             }
@@ -102,31 +76,18 @@ const resources = {
                 "reviews": "Reseñas",
                 "contact": "Contacto"
             },
-            "book_title": "Reserve su estancia",
-            "who_button": "¿Quién viene?",
-            "when_button": "¿Cuándo es tu viaje?",
-            "payment_button": "Método de pago",
-            "who": {
+            "calendar": {
+                "where": "Dónde",
+                "arrival": "Llegada",
+                "departure": "Salida",
+                "who": "Quién"
+            },
+            "info": {
                 "name": "Nombre",
                 "phone": "Teléfono",
-                "email": "Correo electrónico",
-                "guests1": "1 Invitado",
-                "guests2": "2 Invitados",
-                "guests3": "3 Invitados",
-                "guests4": "4 Invitados",
-                "pets": "Mascotas"
+                "email": "Correo electrónico"
             },
-            "when": {
-                "arrival": "Llegada",
-                "departure": "Salida"
-            },
-            "pay": {
-                "name": "Nombre",
-                "card": "Número de tarjeta",
-                "expiration": "Vencimiento (mm/aa)",
-                "security": "Código de seguridad",
-            },
-            "book": "Reservar",
+            "pay": "Pagar",
             "footer": {
                 "copyright": "2024 Riba de Rivers Apartamentos. Todos los derechos reservados."
             }
