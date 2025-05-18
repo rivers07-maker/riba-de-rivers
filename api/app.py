10426ffc3a0851f7a31a16fe226b2058ff5a3bf1
Andres Rios <riosandres91@gmail.com>
2025-05-18 14:11:58 -0400

    fix: Blueprints backend

--- api/app.py
+++ api/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from .contact import blueprint as contact_blueprint
 from .booking import blueprint as booking_blueprint
+from .payment_webhooks import blueprint as payment_event_callback_blueprint
 
 app = Flask(__name__)
 
@@ -9,3 +10,6 @@ app.register_blueprint(blueprint=contact_blueprint, url_prefix='/')
 
 # Register the booking blueprint
 app.register_blueprint(blueprint=booking_blueprint, url_prefix='/')
+
+# Register the booking blueprint
+app.register_blueprint(blueprint=payment_event_callback_blueprint, url_prefix='/')
