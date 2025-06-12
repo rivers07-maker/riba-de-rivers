53d0801f69f50d9f01830253323fc78decd8a92e
Andres Rios <riosandres91@gmail.com>
2025-06-11 23:51:40 -0400

    Fix webhook bug (#7)

--- api/payment_webhooks.py
+++ api/payment_webhooks.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import stripe
 import os
 import logging
-
+import json
 
 # Initialize Blueprint
 blueprint = Blueprint("payment_webhooks", __name__)
@@ -17,23 +17,20 @@ logging.basicConfig(level=logging.INFO)
 @blueprint.route('/payment_event_callback', methods=['POST'])
 def handle_webhook():
     event = None
-    payload = request.get_json()
-    logging.info(f'Headers: {request.headers['Stripe-Signature']}')
-    logging.info(f'Secret: {os.getenv('STRIPE_SECRET_KEY')}')
+    payload = request.get_json() if request.is_json else json.loads(request.data)
+    sig_header = request.headers['Stripe-Signature']
+    secret = os.getenv('STRIPE_SECRET_KEY')
 
     try:
-        event = stripe.Webhook.construct_event(
-            payload=payload,
-            sig_header=request.headers['Stripe-Signature'],
-            secret=os.getenv('STRIPE_SECRET_KEY')
-        )
+        event = stripe.Webhook.construct_event(payload, sig_header, secret)
         logging.info(f'Event: {event}')
         
-        if event.type == 'checkout.session.completed':
+        if event['type'] == 'checkout.session.completed':
             # fulfill_order(event.data.object.id)
             logging.info('Test webhook received')
             
     except ValueError as e:
+        logging.error(f'ValueError: {e}')
         return 'Invalid payload', 400
     except stripe.error.SignatureVerificationError as e:
         return 'Invalid signature', 400
