add76aff884eb614a7820229154c4e1434b25b45
Andres Rios <riosandres91@gmail.com>
2025-06-11 23:45:17 -0400

    refactor: Address webhook payload formatting

--- api/payment_webhooks.py
+++ api/payment_webhooks.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import stripe
 import os
 import logging
-
+import json
 
 # Initialize Blueprint
 blueprint = Blueprint("payment_webhooks", __name__)
@@ -17,11 +17,9 @@ logging.basicConfig(level=logging.INFO)
 @blueprint.route('/payment_event_callback', methods=['POST'])
 def handle_webhook():
     event = None
-    payload = request.body
+    payload = request.get_json() if request.is_json else json.loads(request.data)
     sig_header = request.headers['Stripe-Signature']
     secret = os.getenv('STRIPE_SECRET_KEY')
-    logging.info(f'Headers: {request.headers['Stripe-Signature']}')
-    logging.info(f'Secret: {os.getenv('STRIPE_SECRET_KEY')}')
 
     try:
         event = stripe.Webhook.construct_event(payload, sig_header, secret)
@@ -32,6 +30,7 @@ def handle_webhook():
             logging.info('Test webhook received')
             
     except ValueError as e:
+        logging.error(f'ValueError: {e}')
         return 'Invalid payload', 400
     except stripe.error.SignatureVerificationError as e:
         return 'Invalid signature', 400
